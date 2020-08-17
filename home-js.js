49e146a618fd055e5383b2a5a190d0db53891d40
Nanxin Qin <nancinqin@hotmail.com>
2020-08-16 18:04:55 -0700

    update 0.1

---
+++ home-js.js
@@ -0,0 +1,42 @@
+// HTML elements
+const statusDiv = document.querySelector(".status");
+const resetDiv = document.querySelector(".reset");
+const cellDivs = document.querySelectorAll(".game-cell");
+
+// game variables
+let gameIsLive = true;
+let xIsNext = true;
+
+// event Handlers
+const handleReset = (e) => {
+
+};
+const handleCellClick = (e) => {
+    const classList = e.target.classList;
+    const location = classList[1];
+
+    if(classList[2] == "x" || classList[2] == "o")
+    {
+        return;
+    }
+    if(xIsNext)
+    {
+        classList.add('x');
+        xIsNext = !xIsNext;
+    }
+    else
+    {
+        classList.add('o');
+        xIsNext = !xIsNext;
+    }
+
+};
+
+// event listeners
+resetDiv.addEventListener('click', handleReset);
+
+for (const cellDiv of cellDivs){
+    cellDiv.addEventListener('click', handleCellClick);
+}
+
+
